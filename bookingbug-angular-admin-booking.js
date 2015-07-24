438bf8d48ef2e8e785ccc85f04a41e6a5e9dce09
Ed Clements <ed@bookingbug.com>
2015-07-24 15:15:22 +0300

    Update to version: 0.0.66

--- bookingbug-angular-admin-booking.js
+++ bookingbug-angular-admin-booking.js
@@ -26,11 +26,11 @@
 }).call(this);
 
 (function() {
-  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
+  var __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    __hasProp = {}.hasOwnProperty;
 
-  window.Collection.Client = (function(superClass) {
-    extend(Client, superClass);
+  window.Collection.Client = (function(_super) {
+    __extends(Client, _super);
 
     function Client() {
       return Client.__super__.constructor.apply(this, arguments);
