da94e46cad78fefb89f232f591539bbda217d11a
Luke Ferguson <luke.ferguson@me.com>
2015-11-05 12:15:45 +0000

    Update to version: 0.0.192

--- bookingbug-angular-admin-booking.js
+++ bookingbug-angular-admin-booking.js
@@ -55,6 +55,82 @@
 }).call(this);
 
 (function() {
+  angular.module('BBAdminBooking').directive('bbAdminBooking', function(AdminCompanyService, $log, $compile, $q, PathSvc, $templateCache, $http) {
+    var getTemplate, link, renderTemplate;
+    getTemplate = function(template) {
+      var fromTemplateCache, partial, src;
+      partial = template ? template : 'main';
+      fromTemplateCache = $templateCache.get(partial);
+      if (fromTemplateCache) {
+        return fromTemplateCache;
+      } else {
+        src = PathSvc.directivePartial(partial).$$unwrapTrustedValue();
+        return $http.get(src, {
+          cache: $templateCache
+        }).then(function(response) {
+          return response.data;
+        });
+      }
+    };
+    renderTemplate = function(scope, element, design_mode, template) {
+      return $q.when(getTemplate(template)).then(function(template) {
+        element.html(template).show();
+        if (design_mode) {
+          element.append('<style widget_css scoped></style>');
+        }
+        return $compile(element.contents())(scope);
+      });
+    };
+    link = function(scope, element, attrs) {
+      var config;
+      config = scope.$eval(attrs.bbAdminBooking);
+      config || (config = {});
+      config.admin = true;
+      if (!attrs.companyId) {
+        if (config.company_id) {
+          attrs.companyId = config.company_id;
+        } else if (scope.company) {
+          attrs.companyId = scope.company.id;
+        }
+      }
+      if (attrs.companyId) {
+        return AdminCompanyService.query(attrs).then(function(company) {
+          scope.company = company;
+          scope.initWidget(config);
+          return renderTemplate(scope, element, config.design_mode, config.template);
+        });
+      }
+    };
+    return {
+      link: link,
+      controller: 'BBCtrl'
+    };
+  });
+
+}).call(this);
+
+(function() {
+  angular.module('BBAdminBooking').directive('bbAdminBookingPopup', function(AdminBookingPopup) {
+    var controller, link;
+    controller = function($scope) {
+      return $scope.open = function() {
+        return AdminBookingPopup.open();
+      };
+    };
+    link = function(scope, element, attrs) {
+      return element.bind('click', function() {
+        return scope.open();
+      });
+    };
+    return {
+      link: link,
+      controller: controller
+    };
+  });
+
+}).call(this);
+
+(function() {
   'use strict';
   angular.module('BBAdminBooking').directive('bbAdminBookingClients', function() {
     return {
@@ -167,82 +243,6 @@
     };
     return $scope.edit = function(item) {
       return console.log(item);
-    };
-  });
-
-}).call(this);
-
-(function() {
-  angular.module('BBAdminBooking').directive('bbAdminBooking', function(AdminCompanyService, $log, $compile, $q, PathSvc, $templateCache, $http) {
-    var getTemplate, link, renderTemplate;
-    getTemplate = function(template) {
-      var fromTemplateCache, partial, src;
-      partial = template ? template : 'main';
-      fromTemplateCache = $templateCache.get(partial);
-      if (fromTemplateCache) {
-        return fromTemplateCache;
-      } else {
-        src = PathSvc.directivePartial(partial).$$unwrapTrustedValue();
-        return $http.get(src, {
-          cache: $templateCache
-        }).then(function(response) {
-          return response.data;
-        });
-      }
-    };
-    renderTemplate = function(scope, element, design_mode, template) {
-      return $q.when(getTemplate(template)).then(function(template) {
-        element.html(template).show();
-        if (design_mode) {
-          element.append('<style widget_css scoped></style>');
-        }
-        return $compile(element.contents())(scope);
-      });
-    };
-    link = function(scope, element, attrs) {
-      var config;
-      config = scope.$eval(attrs.bbAdminBooking);
-      config || (config = {});
-      config.admin = true;
-      if (!attrs.companyId) {
-        if (config.company_id) {
-          attrs.companyId = config.company_id;
-        } else if (scope.company) {
-          attrs.companyId = scope.company.id;
-        }
-      }
-      if (attrs.companyId) {
-        return AdminCompanyService.query(attrs).then(function(company) {
-          scope.company = company;
-          scope.initWidget(config);
-          return renderTemplate(scope, element, config.design_mode, config.template);
-        });
-      }
-    };
-    return {
-      link: link,
-      controller: 'BBCtrl'
-    };
-  });
-
-}).call(this);
-
-(function() {
-  angular.module('BBAdminBooking').directive('bbAdminBookingPopup', function(AdminBookingPopup) {
-    var controller, link;
-    controller = function($scope) {
-      return $scope.open = function() {
-        return AdminBookingPopup.open();
-      };
-    };
-    link = function(scope, element, attrs) {
-      return element.bind('click', function() {
-        return scope.open();
-      });
-    };
-    return {
-      link: link,
-      controller: controller
     };
   });
 
