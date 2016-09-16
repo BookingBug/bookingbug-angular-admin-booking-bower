d34d63550b76d79fc1e6c870743af6037c28e570
edclements <ed.clements@gmail.com>
2016-09-16 14:05:06 +0100

    Version v2.0.18

--- bookingbug-angular-admin-booking.js
+++ bookingbug-angular-admin-booking.js
@@ -156,7 +156,7 @@
     };
   });
 
-  angular.module('BBAdminBooking').controller('adminBookingClients', function($scope, $rootScope, $q, AlertService, ValidatorService, ErrorService, $log, BBModel, $timeout, LoadingService, AdminBookingOptions) {
+  angular.module('BBAdminBooking').controller('adminBookingClients', function($scope, $rootScope, $q, AlertService, ValidatorService, ErrorService, $log, BBModel, $timeout, LoadingService, AdminBookingOptions, $translate) {
     var loader;
     $scope.validator = ValidatorService;
     $scope.admin_options = AdminBookingOptions;
@@ -169,13 +169,13 @@
     $scope.sort_by_options = [
       {
         key: 'first_name',
-        name: 'First Name'
+        name: $translate.instant('ADMIN_BOOKING.CUSTOMER.SORT_BY_FIRST_NAME')
       }, {
         key: 'last_name',
-        name: 'Last Name'
+        name: $translate.instant('ADMIN_BOOKING.CUSTOMER.SORT_BY_LAST_NAME')
       }, {
         key: 'email',
-        name: 'Email'
+        name: $translate.instant('ADMIN_BOOKING.CUSTOMER.SORT_BY_EMAIL')
       }
     ];
     $scope.sort_by = $scope.sort_by_options[0].key;
@@ -1021,6 +1021,88 @@
       });
     };
     return assets;
+  });
+
+}).call(this);
+
+(function() {
+  'use strict';
+  angular.module('BBAdminBooking').config(function($translateProvider) {
+    'ngInject';
+    var translations;
+    translations = {
+      ADMIN_BOOKING: {
+        AVAILABILITY: {
+          ANY_PERSON: 'Any person',
+          ANY_RESOURCE: 'Any resource',
+          BACK: 'Back',
+          BOOK: 'Book',
+          CALENDAR: 'Calendar',
+          CONFLICT: 'Availability Conflict',
+          CONFLICT_EXISTS: 'There\'s an availability conflict',
+          CONFLICT_EXISTS_WITH_PERSON: 'with {{ person_name }}',
+          CONFLICT_EXISTS_IN_RESOURCE: 'in {{ resource_name }}',
+          CONFLICT_RESULT_OF: 'This can be the result of:',
+          CONFLICT_REASON_ALREADY_BOOKED: 'The Staff/Resource being booked or blocked already',
+          CONFLICT_REASON_NOT_ENOUGH_TIME: 'Not enough available time to complete the booking before an existing one starts',
+          CONFLICT_REASON_OUTSIDE: 'The selected time being outside of the {{booking_time_step | time_period}} booking time step for {{service_name}}.',
+          CONFLICT_ANOTHER_TIME_OR_OVERBOOK: 'You can either use the calendar to choose another time or overbook.',
+          DAY: 'Day',
+          DAY_3: '3 day',
+          DAY_5: '5 day',
+          DAY_7: '7 day',
+          FIRST_FOUND: 'First available',
+          NOT_FOUND: 'No availability found',
+          NOT_FOUND_TRY_DIFFERENT_TIME_RANGE: 'No availability found, try a different time-range',
+          OVERBOOK: 'Overbook',
+          OVERBOOK_BTN: 'Overbook',
+          OVERBOOK_WARNING: 'Overbooking ignores booking time step and availability constraints to make a booking.',
+          FILTER_BY: 'Filter by:',
+          SELECT_A_TIME: 'Select a time',
+          SELECT_A_TIME_FOR_BOOKING: 'Select a time for the booking.'
+        },
+        CALENDAR: {
+          AFTERNOON: 'Afternoon',
+          EVENING: 'Evening',
+          MORNING: 'Morning'
+        },
+        CUSTOMER: {
+          BACK_BTN: 'Back',
+          CLEAR: 'Clear',
+          CREATE: 'Create Customer',
+          CREATE_BTN: 'Create Customer',
+          CREATE_ONE_INSTEAD: 'Create one instead',
+          EMAIL: 'Email:',
+          FIRST_NAME: 'First name:',
+          LAST_NAME: 'Last name:',
+          MOBILE: 'Mobile:',
+          NO_RESULTS_FOUND: 'No results found',
+          NUMBER: '{CUSTOMERS_NUMBER, plural, =0{no customers} =1{one customer} other{{CUSTOMERS_NUMBER} customers}} found',
+          SEARCH_BY: 'Search by email or name',
+          SELECT_HEADLINE: 'Select a customer',
+          SELECT_BTN: 'Select',
+          SORT_BY: 'Sort by:',
+          SORT_BY_EMAIL: 'Email',
+          SORT_BY_FIRST_NAME: 'First Name',
+          SORT_BY_LAST_NAME: 'Last Name'
+        },
+        QUICK_PICK: {
+          BLOCK_WHOLE_DAY: 'Block whole day',
+          BLOCK_TIME: 'Block time',
+          BOOK: 'Book',
+          FOR: 'For',
+          FROM: 'From',
+          MAKE_BOOKING: 'Make booking',
+          NO: 'No',
+          SELECT: '-- select --',
+          SELECT_A_SERVICE: '-- select a service --',
+          SERVICE: 'Service',
+          TO: 'To',
+          YES: 'Yes'
+        }
+      }
+    };
+    $translateProvider.translations('en', translations);
   });
 
 }).call(this);
