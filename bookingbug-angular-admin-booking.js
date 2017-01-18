24cf33bebb754ab02752aa35dced37705f253407
edclements <ed.clements@gmail.com>
2017-01-18 10:56:31 +0000

    Version v2.1.5

--- bookingbug-angular-admin-booking.js
+++ bookingbug-angular-admin-booking.js
@@ -1358,7 +1358,7 @@
           CLOSEST_TIME_NO_OVERBOOKING: "Looks like that time wasn\'t available. This could just be because it would be outside of their normal schedule. This was the closest time I found. You can select an alternative time, or you can try booking the requested time anyway, however double bookings aren\'t allowed by your company configuration settings",
           CLOSEST_TIME_ALLOW_OVERBOOKING: "Looks like that time wasn\'t available. This could just be because it would be outside of their normal schedule. This was the closest time I found. You can select an alternative time, or you can try booking the requested time anyway",
           CLOSEST_EARLIER_TIME_BTN: "Closest Earlier: {{closest_earlier | datetime: 'LT'}}",
-          CLOSEST_LATER_TIME_BTN: "Closest Later: {{cloest_later | datetime: 'LT'}}",
+          CLOSEST_LATER_TIME_BTN: "Closest Later: {{closest_later | datetime: 'LT'}}",
           REQUESTED_TIME_BTN: "Requested Time: {{requested_time: datetime: 'LT'}}",
           FIND_ANOTHER_TIME_BTN: "Find another time",
           MORNING_HEADER: "@:COMMON.TERMINOLOGY.MORNING",
